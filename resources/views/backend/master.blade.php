eb1e8c12c7e93d24e4dd290f3b53d353822010a3
Mahmudul Elahi <mahmudul.elahi@gmail.com>
2024-05-22 02:33:46 -0700

    add admin dashboard controller

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -45,20 +45,7 @@
 
     <main id="main" class="main">
 
-        <div class="pagetitle">
-            <h1>Blank Page</h1>
-            <nav>
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                    <li class="breadcrumb-item">Pages</li>
-                    <li class="breadcrumb-item active">Blank</li>
-                </ol>
-            </nav>
-        </div><!-- End Page Title -->
-
-        <section class="section">
-            @yield('content')
-        </section>
+        @yield('content')
 
     </main><!-- End #main -->
 
